4545fdf885d68ef96e8c7e80940c74e67c352bd7
Kevin_Stahl <kevin.sthl97@gmail.com>
2023-02-03 00:10:27 +0000

    Config CalcPage model

--- app/Models/CalcPage.php
+++ app/Models/CalcPage.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class CalcPage extends Model
 {
     use HasFactory;
+
+    /** disable incrementing id to string*/
+    public $incrementing = false;
+    protected $keyType = 'string';
+    /** Remove timestamp requirements */
+    public $timestamps = false;
+
 }
