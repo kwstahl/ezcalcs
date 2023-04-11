bf25ac88585a1dcf66ee309aa029420a3b4597ab
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-04-10 21:43:34 -0500

    s
    s

--- resources/views/gitUpdate.php
+++ resources/views/gitUpdate.php
@@ -1,5 +1,6 @@
 <button id = "sync-btn"> Sync Repository </button>
 <strong> meh </strong>
+<strong> stank </strong>
 <script>
     document.getElementById('sync-btn').addEventListener('click', function() {
         let xhr = new XMLHttpRequest();
