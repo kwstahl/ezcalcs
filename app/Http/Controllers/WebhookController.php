a2e2ff3654d8c5233522236b9d569804fd0af093
Kevin_Stahl <kevin.sthl97@gmail.com>
2023-04-11 03:45:02 +0000

    modified:   app/Http/Controllers/WebhookController.php

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -7,10 +7,20 @@ class WebhookController extends Controller
     public function handlePayload(Request $request)
     {
         $payload = $request->getContent();
-
+        // Pull changes from Git repository
         $event = $request->header('X-GitHub-Event');
         if ($event === 'push') {
             exec('cd /var/www/html/ezcalcs && git pull origin main');
         }
+
+        // clear laravel cache
+        \Artisan::call('cache:clear');
+        //update autoloaded files
+        \Artisan::call('optimize');
+        //Reload PHP-FOM or web server
+        // ...
+
+        //return a response to GitHub
+        return response('OK',200);
     }
 }
