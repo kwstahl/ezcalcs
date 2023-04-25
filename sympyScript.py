ffe9c32a732cdd40becf4328c62d5ac273089085
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-04-25 14:18:50 +0000

    new file:   SympyForms.py 	new file:   __pycache__/SympyForms.cpython-310.pyc 	modified:   sympyScript.py

--- sympyScript.py
+++ sympyScript.py
@@ -1,8 +1,10 @@
 import sympy
 import sys
+import json
+import SympyForms
+from sympy import Symbol
 
-data = sys.argv[1]
-
-output = data.upper()
-
-print(output)
+test1 = SympyForms.Formula(sys.argv[1], sys.argv[2])
+test1.substitute_values()
+test1.solve_for_variable()
+print(test1.converted_answer)
