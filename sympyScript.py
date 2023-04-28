5ce9b62f397ce5c7fe61d678db257c2e6f1de03b
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-04-28 21:27:33 +0000

    modified:   sympyScript.py

--- sympyScript.py
+++ sympyScript.py
@@ -4,7 +4,11 @@ import json
 import SympyForms
 from sympy import Symbol
 
-test1 = SympyForms.Formula(sys.argv[1], sys.argv[2])
-test1.substitute_values()
-test1.solve_for_variable()
-print(test1.converted_answer)
+#test1 = SympyForms.Formula(sys.argv[1], sys.argv[2])
+#test1.substitute_values()
+#test1.solve_for_variable()
+#print(test1.converted_answer)
+
+input = sys.argv[1]
+print(input)
+
