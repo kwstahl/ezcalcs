6d1ed348a85f746d81ca63d2912d27bcc9116abc
Kevin_Stahl <kevin.sthl97@gmail.com>
2023-05-03 21:11:02 +0000

    new file:   app/Http/Livewire/VariableInputsForm.php 	new file:   database/.open 	new file:   resources/views/livewire/var-inputs.blade.php 	new file:   resources/views/livewire/variable-inputs-form.blade.php

---
+++ app/Http/Livewire/VariableInputsForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class VariableInputsForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.variable-inputs-form');
+    }
+}
