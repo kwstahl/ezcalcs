6abfbb0a882080764c85ec4ad79f8d1876297e69
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-05-25 20:06:54 +0000

    modified:   resources/views/livewire/page-form.blade.php

--- resources/views/livewire/page-form.blade.php
+++ resources/views/livewire/page-form.blade.php
@@ -3,8 +3,8 @@
 
     @foreach ($variables as $variable_name => $variable_props)
         {{ $variable_name }}:
-        @foreach ($variable_props as $variable_property)
-            {{ $variable_property }}
+        @foreach ($variable_props as $variable_property => $variable_value)
+            {{ $variable_property }}: {{ $variable_value }}
         @endforeach
         <br>
     @endforeach
