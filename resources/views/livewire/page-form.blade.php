96bf9d6222a7f308a87cb3deaba9bfcd85615d52
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-05-25 21:18:22 +0000

    modified:   resources/views/livewire/page-form.blade.php

--- resources/views/livewire/page-form.blade.php
+++ resources/views/livewire/page-form.blade.php
@@ -2,7 +2,7 @@
 
     @foreach ($variables as $variable_name)
 
-        {{ print_r($variable_name) }};<br>
+        {{ print_r($variable_name) }} <br>
         <label>{{ $variable_name['unit'] }}</label> <br>
         <label>{{ $variable_name['sympi_symbol'] }}</label> <br>
         <label>{{ $variable_name['latex_symbol'] }}</label> <br>
