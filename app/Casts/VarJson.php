2be2fa8dc0c9d3ce42c3aa80833968b6cfab8ba8
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-05-25 19:50:45 +0000

    modified:   app/Casts/VarJson.php

--- app/Casts/VarJson.php
+++ app/Casts/VarJson.php
@@ -16,7 +16,7 @@ class VarJson implements CastsAttributes
     {
         $variables = json_decode($value, true);
         foreach ($variables as $variable_name=>$variable_json_properties){
-            json_decode($variable_json_properties, true);
+            json_encode($variable_json_properties, true);
             $variables[$variable_name] = $variable_json_properties;
         }
         return json_encode($variables);
