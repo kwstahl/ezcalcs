ac57ddf50d1d08b562c67070428e8f8d7c0c204e
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-05-31 17:42:42 +0000

    modified:   app/Http/Livewire/PageForm.php

--- app/Http/Livewire/PageForm.php
+++ app/Http/Livewire/PageForm.php
@@ -11,10 +11,12 @@ class PageForm extends Component
     #passed in as array of var_name:var_props, ...,
     public $variables;
     public $variables_as_collection;
+    public $dumped_vars;
 
     public function mount()
     {
         $this->variables_as_collection = collect($this->variables);
+        $this->dumped_vars = $this->variables_as_collection->dump();
     }
 
     public function render()
