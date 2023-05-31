a20ae7290cf168427f6bd4182d0fd651984d51d4
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-05-31 17:38:51 +0000

    modified:   app/Http/Livewire/PageForm.php

--- app/Http/Livewire/PageForm.php
+++ app/Http/Livewire/PageForm.php
@@ -4,11 +4,18 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\CalcPage;
+use Illuminate\Support\Collection;
 
 class PageForm extends Component
 {
     #passed in as array of var_name:var_props, ...,
     public $variables;
+    public $variables_as_collection;
+
+    public function mount()
+    {
+        $this->variables_as_collection = collect($this->variables);
+    }
 
     public function render()
     {
