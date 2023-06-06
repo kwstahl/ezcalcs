bd7db5ebf376d37553af10c12bbc05823e3d1e00
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-06-06 19:40:03 +0000

    modified:   resources/views/livewire/page-form.blade.php

--- resources/views/livewire/page-form.blade.php
+++ resources/views/livewire/page-form.blade.php
@@ -12,7 +12,7 @@
     <br>
 
     <div>
-        <p>{{ $this->variable_unit_retriever('distance/time') }}</p>
+        <p>{{ $this->variable_unit_table_retriever('distance/time') }}</p>
         
     </div>
 
