c644c94a0f3159562ba6eea5f8850a3c201de09c
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-06-07 14:27:27 +0000

    modified:   resources/views/livewire/page-form.blade.php

--- resources/views/livewire/page-form.blade.php
+++ resources/views/livewire/page-form.blade.php
@@ -13,7 +13,7 @@
 
     <div>
         {{ $this->collect_matching_strings('length/time') }}
-        {{ $this->retrieve_complex_units('length/time') }}
+        {{ $this->retrieve_complex_units('length/time')->dump() }}
     </div>
 
 </div>
