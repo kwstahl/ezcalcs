e2a335af5fe06303bc471a7b5900f8707b470a41
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-06-12 20:42:02 +0000

    modified:   resources/views/UnitCreator.blade.php

--- resources/views/UnitCreator.blade.php
+++ resources/views/UnitCreator.blade.php
@@ -1,7 +1,46 @@
 <html>
-<div>
-Heyo this the unit creator page. Welcome.
-</div>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+        <script type="text/javascript" src="/var/www/html/ezcalcs/resources/js/scripts.js"></script>
+        <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+        @livewireStyles
+    </head>
+    <body>
+        <h1>Formula Data</h1>
+        <h2>
+            ID corresponds to the ID that will queried for the unit. Use the unit's name, like 'velocity'<br>
+            base corresponds to the base unit to be checked, like 'm/s'<br>
+            base_breakdown is the most elementary breakdown of the unit. all the way down to the base units, in a formula fashion.<br>
+            symbol is just the symbol of the unit <br>
+            json_units are all the units to be added. <br>
+            Each unit to be added should have the following structure:
+            <ul>
+                <li>name</li>
+                <li>conversion_factor</li>
+                <li>description</li>
+            </ul>
 
+        </h2>
+        <form action="{{ route('unit.store') }}" method="POST">
+            @method('POST')
+            @csrf
+            <input type="submit">
+        </form>
+
+
+        <script src="" async defer></script>
+
+
+
+        <script>
+
+        </script>
+        @livewireScripts
+    </body>
 
 </html>
