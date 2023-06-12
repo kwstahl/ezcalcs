e00c730f84e7cfe491c0b4373b9a05f66e5c913c
Kevin_Stahl <kevin.sthl97@gmail.com>
2023-06-12 20:44:25 +0000

    new file:   app/Http/Livewire/UnitCreatorPreCompiler.php
    	new file:   resources/views/livewire/unit-creator-pre-compiler.blade.php

---
+++ app/Http/Livewire/UnitCreatorPreCompiler.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class UnitCreatorPreCompiler extends Component
+{
+    public function render()
+    {
+        return view('livewire.unit-creator-pre-compiler');
+    }
+}
