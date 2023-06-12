30237084442f19b8db7e0436fbaba3b1e71025ae
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-06-12 15:54:00 +0000

    modified:   config/database.php

--- config/database.php
+++ config/database.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mariadb'),
+    'default' => env('DB_CONNECTION', 'mognodb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -108,6 +108,15 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
+        ],
+
+        'mongodb' => [
+            'driver' => 'mongodb',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE'),
+            'username' => env('DB_USERNAME'),
+            'password' => env('DB_PASSWORD'),
         ]
     ],
 
