7bafaf787b868564ddca961aa4f4dce5fecf6bf4
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-06-14 21:28:37 +0000

    modified:   resources/views/livewire/unit-table.blade.php

--- resources/views/livewire/unit-table.blade.php
+++ resources/views/livewire/unit-table.blade.php
@@ -1,6 +1,6 @@
 <div>
-
-        <select>
+        <label for="unitClasses">Unit Classes</label>
+        <select name="unitClasses" id="unitClasses">
         @foreach($this->unitClasses as $unitClass)
             <option>{{ $unitClass->unit_class }}</option>
         @endforeach
