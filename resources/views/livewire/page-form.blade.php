07a37cd25e8a2e6819cf224f8c0574b0df009548
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-06-26 16:41:51 +0000

    modified:   resources/views/livewire/page-form.blade.php

--- resources/views/livewire/page-form.blade.php
+++ resources/views/livewire/page-form.blade.php
@@ -2,6 +2,7 @@
 
     @foreach($variablesCollection as $index => $variable)
         <div wire:key="variable-field-{{ $index }}">
+            <text>{{ $variable['unit'][$index] }}</text>
             <input type="text" wire:model="variable.{{ $index }}.unit">
         </div>    
     @endforeach
