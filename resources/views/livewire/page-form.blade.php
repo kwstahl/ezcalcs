994516628f5c6bd030c126f470e12df7b49fa75a
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-06-26 16:04:57 +0000

    modified:   app/Http/Livewire/PageForm.php 	new file:   archivedUnitDefucker.php 	modified:   resources/views/livewire/page-form.blade.php

--- resources/views/livewire/page-form.blade.php
+++ resources/views/livewire/page-form.blade.php
@@ -1,19 +1,5 @@
 <div>
 
-    @foreach ($variables as $variable_name)
-        <hr>
-        {{ key($variable_name) }} <br>
-        <label>{{ $variable_name['unit'] }}</label> <br>
-        <label>{{ $variable_name['sympi_symbol'] }}</label> <br>
-        <label>{{ $variable_name['latex_symbol'] }}</label> <br>
-        <label>{{ $variable_name['description'] }}</label> <br>
-        <hr>
-    @endforeach
-    <br>
-
-    <div>
-        {{ $this->baseVariableTables->dump() }}
-        {{ $this->complexVariableTables->dump() }}
-    </div>
+here we go again
 
 </div>
