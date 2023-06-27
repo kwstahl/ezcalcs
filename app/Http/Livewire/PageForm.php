f08992fc909980186ee794fb688a56c3a9d6c857
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-06-27 14:06:30 +0000

    modified:   app/Http/Livewire/PageForm.php

--- app/Http/Livewire/PageForm.php
+++ app/Http/Livewire/PageForm.php
@@ -29,7 +29,7 @@ class PageForm extends Component
             return $item['unit_class'] == $variableUnit;
         })->get('unit_class');
         dump($variableUnit);
-        dump($this->units);
+        dump($this->units['unit_class']);
     }
 
     public function render()
