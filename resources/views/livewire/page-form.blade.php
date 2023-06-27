a9813cee9881ddb9fe78ecb37ae5b7b0325adf39
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-06-27 16:55:12 +0000

    modified:   resources/views/livewire/page-form.blade.php

--- resources/views/livewire/page-form.blade.php
+++ resources/views/livewire/page-form.blade.php
@@ -4,6 +4,7 @@
         <div wire:key="variable-field-{{ $index }}">
             <text>{{ $variable['unit'] }}</text>
             <input type="text" id="{{ $index }}" wire:model="variable.{{ $index }}.unit">
+                {{ $this->createUnitDropdownItems($index, $variable['unit']) }}
             <select>
                 @foreach($unitOptions[$index][0] as $option)
                     <option>{{ $option[0] }}</option>
