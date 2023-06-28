ed388184840be29d75061e636c054c426070ec22
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-06-28 14:27:35 +0000

    modified:   app/Http/Livewire/PageForm.php

--- app/Http/Livewire/PageForm.php
+++ app/Http/Livewire/PageForm.php
@@ -40,7 +40,7 @@ class PageForm extends Component
                     'conversion_to_base',
                 ];
                 $filtered = $unit->only($selectedProperties);
-                $item['unitOptionsCollection']->put($filtered->symbol, $filtered);
+                $item['unitOptionsCollection']->put($filtered['symbol'], $filtered);
             });
         });
 
