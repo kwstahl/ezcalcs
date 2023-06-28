3bebcc49eb585b43c54143356dc76c5cfcfa221a
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-06-28 13:43:02 +0000

    modified:   resources/views/livewire/page-form.blade.php

--- resources/views/livewire/page-form.blade.php
+++ resources/views/livewire/page-form.blade.php
@@ -14,7 +14,7 @@
             <text>{{ $variable['unit'] }}</text>
             <input type="text" id="variableValue.{{ $index }}">
             
-            <select id="variablesCollection.{{ $index }}.unitOptionsCollection" wire:model="variable.{{ $index }}.unitOptionsCollection">
+            <select wire:model="variablesCollection.{{ $index }}.unitOptionsCollection">
                 @foreach($variable['unitOptionsCollection'] as $option)
                     <option>{{ $option['symbol'] }}</option>
                 @endforeach
