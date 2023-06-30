6d0a17f63d6625f45e9e0c5b7519fbc0df4b488b
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-06-30 15:37:14 +0000

    modified:   resources/views/livewire/page-form.blade.php

--- resources/views/livewire/page-form.blade.php
+++ resources/views/livewire/page-form.blade.php
@@ -16,9 +16,12 @@
             
             
             <select wire:model="pyData.{{ $index }}.selection">
+                <!--
             @foreach($unitOptions[$index] as $selection => $value)
                     <option>{{ $selection }}</option>
             @endforeach    
+
+-->
         </select>
         </div>    
         @endforeach
