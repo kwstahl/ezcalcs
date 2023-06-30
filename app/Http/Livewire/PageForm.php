fead0ed67d328449571a278d6b3aaa01e7802db4
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-06-30 11:04:41 -0500

    Update PageForm.php

--- app/Http/Livewire/PageForm.php
+++ app/Http/Livewire/PageForm.php
@@ -39,7 +39,7 @@ class PageForm extends Component
         $this->unitOptions = $this->variablesCollection->map(function($item, $key){
             $variableUnit = $item['unit'];
             $tableUnits = $this->units->where('unit_class', $variableUnit);
-            $item->put($key, $tableUnits);
+            $newCollect = collect([$key => $tableUnits]);
             return $item[$key];
         });
     }
