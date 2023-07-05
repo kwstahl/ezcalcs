9445209a5c299a6a3b143fd545176d0433ab7259
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-07-05 21:48:36 +0000

    modified:   resources/views/livewire/page-form.blade.php

--- resources/views/livewire/page-form.blade.php
+++ resources/views/livewire/page-form.blade.php
@@ -19,7 +19,7 @@
             <select wire:model="pyData.{{ $variableName }}.Unit">
                 <option selected>{{ $variableName }}</option>
                 @foreach($variablesCollection[$variableName]['unitOptions'] as $subUnitIndex => $subUnit)
-                    <option value={{ $subUnit['conversion_to_base'] }}>{{ $subUnit['symbol'] }} </option>
+                    <option value="{{ $subUnit['conversion_to_base'] }}">{{ $subUnit['symbol'] }} </option>
                 @endforeach
         </select>
         </div>    
