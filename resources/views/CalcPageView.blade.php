f9be3258f6d9c656c9eddc0d8f80077cf18e4a5e
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-07-07 16:02:17 +0000

    modified:   resources/views/CalcPageView.blade.php

--- resources/views/CalcPageView.blade.php
+++ resources/views/CalcPageView.blade.php
@@ -11,7 +11,7 @@
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="">
-        <script src="jquery-3.6.4.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
         @livewireStyles
     </head>
     <body>
