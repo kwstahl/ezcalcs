c9f5d7d6ae1d4f6b4e1c24471403611730a34048
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-07-11 17:21:39 +0000

    modified:   resources/views/UnitCreator.blade.php

--- resources/views/UnitCreator.blade.php
+++ resources/views/UnitCreator.blade.php
@@ -33,7 +33,8 @@
         @csrf
         <input type="text" name="id">id</input><br>
         <input type="text" name="base_unit">base</input><br>
-        <input type="text" name="conversion_to_base">base_breakdown</input><br>
+        <input type="text" name="base_breakdown"> base breakdown</input><br>
+        <input type="text" name="conversion_to_base">conversion to base</input><br>
         <input type ="text" name="description">description</input><br>
         <input type="text" name="unit_class">unit_class</input><br>
         <input type="text" name="symbol">symbol</input><br>
