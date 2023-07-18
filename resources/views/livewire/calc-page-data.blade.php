f50af20ee2ffb550ad1b07833d43f4d98edd9e33
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-07-18 20:13:10 +0000

    modified:   resources/views/livewire/calc-page-data.blade.php

--- resources/views/livewire/calc-page-data.blade.php
+++ resources/views/livewire/calc-page-data.blade.php
@@ -35,10 +35,10 @@
                         @foreach($eqnData['variables_json'] as $variableName => $variable)
                             <ul wire:key="var-field-{{ $variableName }}">
                             {{ $variableName }}<br>
-                                <li>Unit<input type="text" wire:model="calcPages.{{ $eqnData->id }}.variables_json.{{ $variableName }}.unit"> </li>
-                                <li>Sympi Symbol<input type="text" wire:model="calcPages.{{ $eqnData->id }}.variables_json.{{ $variableName }}.sympi_symbol"> </li>
-                                <li>Latex Symbol<input type="text" wire:model="calcPages.{{ $eqnData->id }}.variables_json.{{ $variableName }}.latex_symbol"> </li>
-                                <li>Description<input type="text" wire:model="calcPages.{{ $eqnData->id }}.variables_json.{{ $variableName }}.description"> </li>
+                                <li>Unit<input type="text" wire:model="calcPages.{{ $eqnName }}.variables_json.{{ $variableName }}.unit"> </li>
+                                <li>Sympi Symbol<input type="text" wire:model="calcPages.{{ $eqnName}}.variables_json.{{ $variableName }}.sympi_symbol"> </li>
+                                <li>Latex Symbol<input type="text" wire:model="calcPages.{{ $eqnName }}.variables_json.{{ $variableName }}.latex_symbol"> </li>
+                                <li>Description<input type="text" wire:model="calcPages.{{ $eqnName }}.variables_json.{{ $variableName }}.description"> </li>
 
                             </ul>
 
