1994ac94622e2e307c60a03a9e379905cf2c758c
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-07-19 14:42:53 +0000

    modified:   app/Http/Livewire/CalcPageData.php

--- app/Http/Livewire/CalcPageData.php
+++ app/Http/Livewire/CalcPageData.php
@@ -21,7 +21,9 @@ class CalcPageData extends Component
     public function mount()
     {
         $this->calcPages = CalcPage::all();
-        $this->calcPages = $this->calcPages->collapse();
+        $this->calcPages = $this->calcPages->transform(function($item, $key){
+            return [$item->id => $item];
+        });
         $this->variables = collect();
         //$this->calcPages->each(function($formula, $formulaName){
         //    $this->variables->put($formula['id'], $formula['variables_json']);
