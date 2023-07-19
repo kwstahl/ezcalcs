c1d55821237e1ba0f1588fe9d7f985f10d546473
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-07-19 15:08:23 +0000

    modified:   resources/views/livewire/calc-page-data.blade.php

--- resources/views/livewire/calc-page-data.blade.php
+++ resources/views/livewire/calc-page-data.blade.php
@@ -33,7 +33,7 @@
                     <td>
                         <hr>
 
-                        {{ dd($calcPages) }}
+                        {{ dd($eqnData->id) }}
                         @foreach($calcPages[$eqnName]['variables_json'] as $variableName => $variable)
                             <ul wire:key="var-field-{{ $variableName }}">
                             {{ $variableName }}<br>
