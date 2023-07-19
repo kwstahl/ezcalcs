ec87040953df35e937ce1f901d8f979595352eb2
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-07-19 14:56:41 +0000

    modified:   app/Http/Livewire/CalcPageData.php

--- app/Http/Livewire/CalcPageData.php
+++ app/Http/Livewire/CalcPageData.php
@@ -22,7 +22,7 @@ class CalcPageData extends Component
     {
         $this->calcPages = CalcPage::all();
         $this->calcPages->transform(function($item, $key){
-            return [$item['id'] => $item->get()];
+            return [$item->id => $item->get()];
         });
         $this->variables = collect();
         //$this->calcPages->each(function($formula, $formulaName){
