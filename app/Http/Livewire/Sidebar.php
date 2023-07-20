992f6211a196314d9bfc333d380deabd4d1d7c92
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-07-20 19:21:34 +0000

    modified:   app/Http/Livewire/Sidebar.php

--- app/Http/Livewire/Sidebar.php
+++ app/Http/Livewire/Sidebar.php
@@ -27,7 +27,6 @@ class Sidebar extends Component
     public function setUrl($pageId)
     {
         $id = $this->calcPages->where('id', $pageId);
-        dd($id);
         echo url("/eqn/{$id}");
     }
 
