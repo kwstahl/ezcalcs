63a4d1e0995b3db544ae4b9a0abc64afb760c399
Kevin_Stahl <kevin.sthl97@gmail.com>
2023-07-20 13:51:06 +0000

    new file:   app/Http/Livewire/Sidebar.php

---
+++ app/Http/Livewire/Sidebar.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Sidebar extends Component
+{
+    public function render()
+    {
+        return <<<'blade'
+            <div>
+                {{-- Be like water. --}}
+            </div>
+        blade;
+    }
+}
