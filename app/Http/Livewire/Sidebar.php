f270ae97077ab3bf53136386e8c6ebea7cbd7a3e
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-07-20 16:02:27 +0000

    modified:   app/Http/Livewire/Sidebar.php 	modified:   resources/views/CalcPageView.blade.php modified:   resources/views/livewire/sidebar.blade.php

--- app/Http/Livewire/Sidebar.php
+++ app/Http/Livewire/Sidebar.php
@@ -3,9 +3,21 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\CalcPage;
+
 
 class Sidebar extends Component
 {
+    public $formulas;
+    public $topics;
+    public $topicsArray;
+
+    public function mount()
+    {
+        $this->formulas = CalcPage::all()->pluck('topic', 'id', 'formula_name');
+        $this->topics = $this->formulas->groupBy('topic');
+    }
+
     public function render()
     {
         return view('livewire.sidebar');
