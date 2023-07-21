d8edd2bfe21704584431d623129810fb294ab45f
Kevin_Stahl <kevin.sthl97@gmail.com>
2023-07-21 21:48:08 +0000

    new file:   app/Http/Livewire/Information.php 	new file:   resources/views/livewire/information.blade.php

---
+++ app/Http/Livewire/Information.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Information extends Component
+{
+    public function render()
+    {
+        return view('livewire.information');
+    }
+}
