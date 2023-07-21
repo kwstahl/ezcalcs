1df92ed6d4aeeef64abc900b146558409cf2e86c
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-07-21 14:47:21 +0000

    modified:   resources/views/livewire/sidebar.blade.php

--- resources/views/livewire/sidebar.blade.php
+++ resources/views/livewire/sidebar.blade.php
@@ -1,5 +1,5 @@
-<div class="col-lg-2 col-md-3 col-sm-3 col-xl-2 ">
-    <div class="accordion" id="accordionExample">
+<div class="col-lg-2 col-md-3 col-sm-3 col-xl-2 p-0">
+    <div class="accordion accordion-flush" id="accordionExample">
         <!-- Physics Group -->
         @foreach($pagesByTopic as $topic => $pageByTopic)
         <div class="accordion-item">
