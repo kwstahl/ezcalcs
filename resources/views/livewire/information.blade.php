a4b9684667f8f3c66783514a3bc91e58883e872e
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-07-24 14:29:16 +0000

    modified:   resources/views/livewire/information.blade.php

--- resources/views/livewire/information.blade.php
+++ resources/views/livewire/information.blade.php
@@ -1,11 +1,12 @@
 <nav>
     <div class="nav nav-tabs" id="nav-tab" role="tablist">
       <button class="nav-link active" id="nav-description-tab" data-bs-toggle="tab" data-bs-target="#nav-description" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Description</button>
-      <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Profile</button>
-      <button class="nav-link" id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Contact</button>
-      <button class="nav-link" id="nav-disabled-tab" data-bs-toggle="tab" data-bs-target="#nav-disabled" type="button" role="tab" aria-controls="nav-disabled" aria-selected="false" disabled>Disabled</button>
+        @foreach($variables as $variableName => $variable)
+            <button class="nav-link" id ="nav-{{ $variableName }}-tab" data-bs-toggle="tab" data-bs-target="#nav-{{ $variableName }}" type="button" role="tab"> {{ $variableName }} </button>
+        @endforeach
     </div>
   </nav>
+
   <div class="tab-content" id="nav-tabContent">
     <div class="tab-pane fade show active" id="nav-description" role="tabpanel" aria-labelledby="nav-description-tab" tabindex="0"> {{ $description }} </div>
     <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">...</div>
