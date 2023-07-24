2b370b6e0f2159e41f5670239aad8c116e6b5cfe
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-07-24 14:12:51 +0000

    modified:   resources/views/livewire/information.blade.php

--- resources/views/livewire/information.blade.php
+++ resources/views/livewire/information.blade.php
@@ -1,3 +1,17 @@
-<div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+<div class="d-flex align-items-start">
+    <div class="nav flex-column nav-pills me-3" id="v-pills-tab" role="tablist">
+        <button class="nav-link" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="v-pills-home" type="button" role="tablist">
+            Description
+        </button>
+        <button class="nav-link" id="v-pills-home-tab" data-bs-toggle="pill" data-bs-target="v-pills-var1" type="button" role="tablist">
+            Description
+        </button>
+    </div>
+
+    <div class="tab-content" id="v-pills-tabContent">
+        <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" tabindex="0">What it do</div>
+        <div class="tab-pane fade" id="v-pills-var1" role="tabpanel"></div>
+    </div>
+
+
 </div>
