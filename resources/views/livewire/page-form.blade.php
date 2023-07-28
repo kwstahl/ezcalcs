29a8793291bdf58acd92c6f0ed58288a01fa4731
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-07-28 20:58:30 +0000

    modified:   resources/views/livewire/page-form.blade.php

--- resources/views/livewire/page-form.blade.php
+++ resources/views/livewire/page-form.blade.php
@@ -30,7 +30,7 @@
                 <select class="form-select" wire:model="boundDataForSympy.{{ $variableName }}.unit_conversion">
                     <option selected>{{ $variableName }}</option>
                     @foreach ($variable['unitOptions'] as $subUnitIndex => $subUnit)
-                        <option value="{{ $subUnit['conversion_to_base'] }}">{{ $subUnit['symbol'] }} </option>
+                        <option wire:key="{{ $subUnit['symbol'] }}" value="{{ $subUnit['conversion_to_base'] }}">{{ $subUnit['symbol'] }} </option>
                     @endforeach
                 </select>
 
