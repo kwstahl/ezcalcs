f4d3141c0bada4816e18e28bd9042530f8e00ce5
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-07-28 21:26:24 +0000

    modified:   resources/views/livewire/page-form.blade.php

--- resources/views/livewire/page-form.blade.php
+++ resources/views/livewire/page-form.blade.php
@@ -47,6 +47,6 @@
 </div>
 
 <h1 class="display-6 text-align-center">
-    Solve For: {{ $variableToSolveFor }} in  <br> {{ $answer }} <br>
+    Solve For: {{ $variableToSolveFor }} in  <br> {{ $variableToSolveForUnit }} <br>
 </h1>
 </div>
