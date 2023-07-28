95683a2fbb52cc8649497ce71183575cb27372af
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-07-28 17:06:16 +0000

    modified:   resources/views/CalcPageView.blade.php

--- resources/views/CalcPageView.blade.php
+++ resources/views/CalcPageView.blade.php
@@ -39,7 +39,6 @@
 
 <body>
     <!--[if lt IE 7]>
-=======
     <body>
         <!--[if lt IE 7]>
             <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
