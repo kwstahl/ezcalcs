bf6a56e22eb02bd0ea9fc94ce3d3f6aa9c799548
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-09-23 17:08:59 +0000

    modified:   public/sympyScript.py

--- public/sympyScript.py
+++ public/sympyScript.py
@@ -7,6 +7,6 @@ from sympy import Symbol
 
 
 test1 = SympyForms.Formula(sys.argv[1], sys.argv[2])
-test1.substitute_values()
+test1.substitute_converted_values()
 test1.solve_for_variable()
 print(round(test1.converted_answer, 6))
