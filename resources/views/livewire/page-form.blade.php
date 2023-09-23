f28321f34369ea773846feadc7664242c8ffb6b4
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-09-23 16:17:31 +0000

    modified:   resources/views/livewire/page-form.blade.php

--- resources/views/livewire/page-form.blade.php
+++ resources/views/livewire/page-form.blade.php
@@ -33,7 +33,7 @@
                         @endforeach
                     </select>
 
-                    <label> Unit: {{ $variable['unit_class'] }}</label>
+                    <label> Unit: {{ $unitsForVariables[$variableName]['unit_class'] }}</label>
                 </div>
             </div>
             <hr>
