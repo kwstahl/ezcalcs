193411cb8a2880dc32265025cb5be044b41b3943
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-09-28 22:27:28 +0000

    modified:   app/Http/Livewire/PageForm/Variable.php 	modified:   resources/views/livewire/page-form.blade.php

--- app/Http/Livewire/PageForm/Variable.php
+++ app/Http/Livewire/PageForm/Variable.php
@@ -7,6 +7,7 @@ use Livewire\Component;
 class Variable extends Component
 {
     public $variables_json;
+    public $variableToSolveFor;
     public function render()
     {
         return view('livewire.page-form.variable');
--- resources/views/livewire/page-form.blade.php
+++ resources/views/livewire/page-form.blade.php
@@ -46,7 +46,7 @@
             <hr>
         @endforeach
 
-        @livewire('page-form.variable', ['variables_json' => $variables_json])
+        @livewire('page-form.variable', ['variables_json' => $variables_json, 'variableToSolveFor' => $variableToSolveFor])
 
         @if ($errors->any())
             <div class="alert alert-danger">
