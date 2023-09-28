8facbcde3f82a3bb1254a1e1eb304f92e73e69f1
Kevin_Stahl <kevin.sthl97@gmail.com>
2023-09-28 22:12:22 +0000

    new file:   app/Http/Livewire/PageForm/Constant.php 	new file:   app/Http/Livewire/PageForm/Unitless.php 	new file:   app/Http/Livewire/PageForm/Variable.php 	new file:   resources/views/livewire/page-form/constant.blade.php 	new file:   resources/views/livewire/page-form/unitless.blade.php 	new file:   resources/views/livewire/page-form/variable.blade.php

---
+++ app/Http/Livewire/PageForm/Constant.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\PageForm;
+
+use Livewire\Component;
+
+class Constant extends Component
+{
+    public function render()
+    {
+        return view('livewire.page-form.constant');
+    }
+}
---
+++ app/Http/Livewire/PageForm/Variable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\PageForm;
+
+use Livewire\Component;
+
+class Variable extends Component
+{
+    public function render()
+    {
+        return view('livewire.page-form.variable');
+    }
+}
