8d207fb646e975d27410315f2db9f04250407a96
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-09-28 21:22:44 +0000

    modified:   app/Http/Livewire/UnitTable.php

--- app/Http/Livewire/UnitTable.php
+++ app/Http/Livewire/UnitTable.php
@@ -48,7 +48,7 @@ class UnitTable extends Component
 
     public function mount()
     {
-        $this->unitClasses = Unit::pluck('unit_class')->unique()->sortBy(['unit_class', 'asc']);
+        $this->unitClasses = Unit::pluck('unit_class')->sortBy(['unit_class', 'asc'])->unique();
         $this->baseUnits = Unit::pluck('base_unit')->unique();
         $this->units = Unit::all();
 
