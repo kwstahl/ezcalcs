a5e224d41ee799bfec254217f5c7cb093959c778
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-09-28 18:17:07 +0000

    modified:   app/Traits/CalcPageHelpers.php

--- app/Traits/CalcPageHelpers.php
+++ app/Traits/CalcPageHelpers.php
@@ -12,6 +12,7 @@ trait CalcPageHelpers
 {
     public function sortAscending(&$model, $field)
     {
+        collect($model);
         $model = $model->sortBy([
             [$field, 'asc'],
         ]);
