146728b5680b47cb930d63962f1a445a9008f115
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-09-28 18:44:33 +0000

    modified:   app/Traits/CalcPageHelpers.php

--- app/Traits/CalcPageHelpers.php
+++ app/Traits/CalcPageHelpers.php
@@ -6,12 +6,13 @@ use App\Models\CalcPage;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Unit;
 
 trait CalcPageHelpers 
 {
     public function sortAscending(&$model, $field)
     {
-        $model = "\\App\\Models\\".$model::all()->sortBy([$field, 'asc']);     
+        $model = $model::all()->sortBy([$field, 'asc']);     
         $this->render();
     }
 
