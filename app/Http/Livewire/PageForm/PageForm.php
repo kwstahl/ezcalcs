0c83181e36e870a8124b3f38d93be98624bfddf3
Kevin_Stahl <kevin.sthl97@gmail.com>
2023-09-29 18:41:26 +0000

    new file:   app/Http/Livewire/PageForm/PageForm.php 	new file:   resources/views/livewire/page-form/page-form.blade.php

---
+++ app/Http/Livewire/PageForm/PageForm.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\PageForm;
+
+use Livewire\Component;
+
+class PageForm extends Component
+{
+    public function render()
+    {
+        return view('livewire.page-form.page-form');
+    }
+}
