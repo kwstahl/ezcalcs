57e1f1210822ce265bfe4765ca3882b30ef94e54
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-09-29 21:38:42 +0000

    modified:   resources/views/livewire/page-form/page-form-test.blade.php

--- resources/views/livewire/page-form/page-form-test.blade.php
+++ resources/views/livewire/page-form/page-form-test.blade.php
@@ -5,7 +5,7 @@
         </x-calc-page.test-a>
 
             @foreach($variables_json as $variableName => $variable)
-                <x-calc-page.test-a :message="{{$variableName}}"/>
+                <x-calc-page.test-a :message="$variableName"/>
             @endforeach
 
 
