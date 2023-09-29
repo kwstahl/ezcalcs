f7a9995f56dafb1d081d44d5872b3ef7074ab5e8
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-09-29 21:28:43 +0000

    modified:   resources/views/livewire/page-form/page-form-test.blade.php

--- resources/views/livewire/page-form/page-form-test.blade.php
+++ resources/views/livewire/page-form/page-form-test.blade.php
@@ -23,7 +23,7 @@
                         <div class="input-group-text">
                             <input class="form-check-input mt-0" type="radio" name="solveFor" value="{{ $variableName }}"
                                 wire:model="variableToSolveFor">
-                        </div>
+                            </div>
 
                         <!-- Input Text -->
                         <div class="form-floating">
