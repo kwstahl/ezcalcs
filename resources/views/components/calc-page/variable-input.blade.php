a9ebd10b58a3ab32faacc6fff9901f47e943e54c
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-09-30 19:34:16 +0000

    modified:   resources/views/components/calc-page/variable-input.blade.php

--- resources/views/components/calc-page/variable-input.blade.php
+++ resources/views/components/calc-page/variable-input.blade.php
@@ -3,3 +3,5 @@
 <input class="form-control" type="text" name="{{ $variableName }}"
     wire:model.defer="variableInputData.{{ $variableName }}.Value"
     @if ($variableToSolveFor === $variableName) disabled readonly @endif>
+
+<label wire:ignore>{{ $variableName }} ({{ $variable['latex_symbol'] }}) </label>
