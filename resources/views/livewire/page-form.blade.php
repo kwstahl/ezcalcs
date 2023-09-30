56b66d839f607bf0d69b36cc06ff0bfe6e969acc
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-09-30 19:22:13 +0000

    modified:   resources/views/livewire/page-form.blade.php

--- resources/views/livewire/page-form.blade.php
+++ resources/views/livewire/page-form.blade.php
@@ -10,8 +10,7 @@
 
                         <!--  Radio  -->
                         <div class="input-group-text">
-                            <input class="form-check-input mt-0" type="radio" name="solveFor" value="{{ $variableName }}"
-                                wire:model="variableToSolveFor">
+                            <x-calc-page.radio/>
                         </div>
 
                         <!-- Input Text -->
