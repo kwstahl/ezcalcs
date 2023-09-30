402b71dde48a7951214ea0540e498ae326e55d1f
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-09-30 19:07:41 +0000

    modified:   resources/views/components/calc-page/test-a.blade.php

--- resources/views/components/calc-page/test-a.blade.php
+++ resources/views/components/calc-page/test-a.blade.php
@@ -1,10 +1,9 @@
-@props(['variableName'])
+@props(['variableName', 'variable'])
 
-<div class="input-group-text">
-    <input 
-    class="form-check-input mt-0" 
-    type="radio" 
-    name="solveFor" 
-    value="{{ $variableName }}"
-    wire:model="variableToSolveFor">
+<div class="form-floating">
+    <input class="form-control" type="text" name="{{ $variableName }}"
+        wire:model.defer="variableInputData.{{ $variableName }}.Value"
+        @if ($variableToSolveFor === $variableName) disabled
+                                    readonly @endif>
+    <label wire:ignore>{{ $variableName }} ({{ $variable['latex_symbol'] }}) </label>
 </div>
