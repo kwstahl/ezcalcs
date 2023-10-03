f9ff1bd177e1c820d9017b6edee7aa37f502773f
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-10-03 21:02:03 +0000

    modified:   resources/views/components/calc-page/unitless-layout.blade.php

--- resources/views/components/calc-page/unitless-layout.blade.php
+++ resources/views/components/calc-page/unitless-layout.blade.php
@@ -1,3 +1,18 @@
-<div>
-    <!-- It is not the man who has too little, but the man who craves more, that is poor. - Seneca -->
+@props(['variableName', 'variableToSolveFor', 'variable', 'unitOptions'])
+
+<div class="row gx-1 gy-1 mb-1 p-2" wire:key="variable-field-{{ $variableName }}">
+    <div class="col-8">
+        <div class="input-group">
+
+            <!--  Radio  -->
+            <div class="input-group-text">
+                <x-calc-page.radio/>
+            </div>
+
+            <!-- Input Text -->
+            <div class="form-floating">
+                <x-calc-page.variable-input/>
+            </div>
+        </div>
+    </div>
 </div>
