83a53807d4c78298a6866cc29c6c01f1d0a7ddd5
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-10-04 21:29:00 +0000

    modified:   resources/views/components/calc-page/dropdown.blade.php

--- resources/views/components/calc-page/dropdown.blade.php
+++ resources/views/components/calc-page/dropdown.blade.php
@@ -1,6 +1,6 @@
 @aware(['variableName', 'bind', 'variable', 'type'])
 
-<div class="$attributes->merge(['class' => 'col-4 form-floating'])">
+<div class="col-4 form-floating">
     <select type="$type" class="form-select" id="{{ $variableName }}" wire:model.defer="{{ $bind }}" wire:ignore>
         <!-- Options -->
         {{ $slot }}
