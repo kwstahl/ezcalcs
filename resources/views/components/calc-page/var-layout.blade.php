7eebd5e58245594c8453a09d80704041d37ca932
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-10-04 21:28:12 +0000

    modified:   resources/views/components/calc-page/var-layout.blade.php

--- resources/views/components/calc-page/var-layout.blade.php
+++ resources/views/components/calc-page/var-layout.blade.php
@@ -18,7 +18,7 @@
 
 
     <!-- Dropdown list -->
-    <x-calc-page.dropdown class="d-none" :$variable :$variableName bind="variableInputData.{{ $variableName }}.unit_conversion">
+    <x-calc-page.dropdown :$variable :$variableName bind="variableInputData.{{ $variableName }}.unit_conversion">
         <!-- Options -->
         <option selected>{{ $variableName }}</option>
         @foreach ($unitOptions[$variableName] as $unitIndex => $unit)
