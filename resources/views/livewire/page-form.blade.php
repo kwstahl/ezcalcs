4415bfedea9d727b9418da5bc68a79140c20d0b6
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-10-09 21:49:26 +0000

    modified:   resources/views/livewire/page-form.blade.php

--- resources/views/livewire/page-form.blade.php
+++ resources/views/livewire/page-form.blade.php
@@ -26,7 +26,7 @@
                         -->
 
                         <div class="dropdown col-4">
-                            <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown" wire:ignore>
+                            <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown">
                                 @isset($variableInputData[$variableName]['unit_conversion'])
                                     {{ $variableInputData[$variableName]['unit_conversion'] }}
                                 @endisset
