2532d8a83952a7023b848dfbfe1a7c34befaae87
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-10-10 21:21:27 +0000

    modified:   app/Http/Livewire/PageForm.php 	modified:   resources/views/livewire/page-form.blade.php

--- app/Http/Livewire/PageForm.php
+++ app/Http/Livewire/PageForm.php
@@ -91,8 +91,8 @@ class PageForm extends Component
     public function setUnitInputData($variableName, $unit)
     {
         $new = $this->variableInputData[$variableName];
-        $new['unit_conversion'] = $unit['conversion_to_base'];
-        $new['symbol'] = $unit['symbol'];
+        $new['unit_conversion'] = $this->units[$unit]['conversion_to_base'];
+        $new['symbol'] = $this->units[$unit]['symbol'];
         $this->variableInputData[$variableName] = $new;
     }
 
