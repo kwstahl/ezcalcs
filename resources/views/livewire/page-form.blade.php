18d5ea3e86e79ea09bbe01c582f51803b4e7874c
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-10-11 21:14:01 +0000

    modified:   resources/views/livewire/page-form.blade.php

--- resources/views/livewire/page-form.blade.php
+++ resources/views/livewire/page-form.blade.php
@@ -10,31 +10,29 @@
 
                         <!-- Dropdown list -->
                         <!--
-                        <div class="col-4 form-floating">
-                            <select class="form-select" id=" $variableName "
-                                wire:model.defer="variableInputData. $variableName .unit_conversion" wire:ignore>
-                                <option selected> $variableName </option>
-                                foreach ($unitOptions[$variableName] as $unitIndex => $unit)
-                                    <option value=" $unit['conversion_to_base'] ">
-                                         $unit['symbol']
-                                    </option>
-                                endforeach
-                            </select>
-                            <label> Unit:  $variable['unit'] </label>
-                        </div>
+                                <div class="col-4 form-floating">
+                                    <select class="form-select" id=" $variableName "
+                                        wire:model.defer="variableInputData. $variableName .unit_conversion" wire:ignore>
+                                        <option selected> $variableName </option>
+                                        foreach ($unitOptions[$variableName] as $unitIndex => $unit)
+                                            <option value=" $unit['conversion_to_base'] ">
+                                                 $unit['symbol']
+                                            </option>
+                                        endforeach
+                                    </select>
+                                    <label> Unit:  $variable['unit'] </label>
+                                </div>
 
-                        -->
+                                -->
 
-                        <div class="col-2">
-                            <div>
-                                @isset($variableInputData[$variableName]['unit_symbol'])
-                                    <span>{{ $variableInputData[$variableName]['unit_symbol']}}</span>
-                                @endisset
+                        <div class="col-4">
+                            @isset($variableInputData[$variableName]['unit_symbol'])
+                                <span>{{ $variableInputData[$variableName]['unit_symbol'] }}</span>
+                            @endisset
 
-                                @empty($variableInputData[$variableName]['unit_symbol'])
-                                    Select a Unit
-                                @endempty
-                            </div>
+                            @empty($variableInputData[$variableName]['unit_symbol'])
+                                Select a Unit
+                            @endempty
 
                             <button class="btn dropdown-toggle" type="button" data-bs-toggle="dropdown">
                                 <span class="visually-hidden">Toggle Dropdown</span>
@@ -43,7 +41,8 @@
                             <ul class="dropdown-menu" wire:ignore>
                                 @foreach ($unitOptions[$variableName] as $unitIndex => $unit)
                                     <li>
-                                        <button class="dropdown-item" type="button" wire:click="setUnitInputData('{{$variableName}}', '{{$unitIndex}}')">
+                                        <button class="dropdown-item" type="button"
+                                            wire:click="setUnitInputData('{{ $variableName }}', '{{ $unitIndex }}')">
                                             {{ $unit['symbol'] }}
                                         </button>
                                     </li>
