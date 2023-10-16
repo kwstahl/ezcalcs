cf1711bc57046a99bbe9b7f383190d4bdadf007f
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-10-16 21:31:07 +0000

    modified:   app/Http/Livewire/PageForm/PageFormTest.php

--- app/Http/Livewire/PageForm/PageFormTest.php
+++ app/Http/Livewire/PageForm/PageFormTest.php
@@ -4,7 +4,7 @@ namespace App\Http\Livewire\PageForm;
 
 use Livewire\Component;
 use App\Models\Unit;
-use App\Variables\Variable;
+use App\Classes\Variable\Variables;
 use Illuminate\Support\Facades\Process;
 
 class PageFormTest extends Component
