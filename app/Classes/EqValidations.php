8d04fdf61051264b078c2951776a4c133ca0a0ac
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-10-27 19:37:06 +0000

    modified:   app/Classes/EqValidations.php

--- app/Classes/EqValidations.php
+++ app/Classes/EqValidations.php
@@ -10,7 +10,7 @@ class EqValidations
 
     protected function setPropertiesForValidation($attributes_to_validate){
         foreach($attributes_to_validate as $attribute=>$value){
-            $this->$attribute;
+            $this->$attribute = $attribute;
         }
     }
 }
