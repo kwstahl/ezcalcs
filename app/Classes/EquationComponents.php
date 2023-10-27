230dff283a864f637c96920ff6b62c38525db857
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-10-27 18:38:42 +0000

    modified:   app/Classes/EquationComponents.php

--- app/Classes/EquationComponents.php
+++ app/Classes/EquationComponents.php
@@ -13,8 +13,6 @@ abstract class EquationComponents
     public $attributes_array;
     public $attribute_validations;
 
-    abstract public function __construct(Array $attributes_array);
-
     protected function setPropertiesFrom_attributes_array(Array $attributes_array)
     {
         foreach($attributes_array as $attribute=>$value){
