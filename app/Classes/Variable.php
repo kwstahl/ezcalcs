a10c2f84eddc179c970a4e636bb66e6865e4c8ef
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-10-27 18:37:52 +0000

    modified:   app/Classes/Variable.php

--- app/Classes/Variable.php
+++ app/Classes/Variable.php
@@ -14,7 +14,7 @@ class Variable extends EquationComponents
     public $sympy_symbol;
     public $latex_symbol;
 
-    public function __construct(Array $attributes_array, String $name=null)
+    public function __construct(String $name=null, Array $attributes_array)
     {
         $this->name = $name;
         $this->attributes_array = $attributes_array;
