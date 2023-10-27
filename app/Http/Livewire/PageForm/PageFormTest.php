35fe4d7f6749a1d31fd972d0ec7d965a4939705c
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-10-27 20:05:36 +0000

    modified:   app/Http/Livewire/PageForm/PageFormTest.php

--- app/Http/Livewire/PageForm/PageFormTest.php
+++ app/Http/Livewire/PageForm/PageFormTest.php
@@ -147,7 +147,7 @@ class PageFormTest extends Component
         );
 
         $vald = new EqValidations;
-        $vald->setSameValidationRulesOnAll(['ham'=>'flam', 'bam'=>'gam'], 'stank', 'flank');
+        $vald->setSameValidationRulesOnAll(['ham'=>'flam', 'bam'=>'gam'], 'stank', 'flank', 'fool');
 
         dd($vald->ham);
     }
