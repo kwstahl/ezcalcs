2234496e1655e977768372f9ba2df26bd8115b40
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-10-27 18:33:38 +0000

    modified:   app/Classes/EquationComponents.php 	new file:   app/Classes/PageHelpers.php

--- app/Classes/EquationComponents.php
+++ app/Classes/EquationComponents.php
@@ -10,30 +10,16 @@ abstract class EquationComponents
 {
     public $type;
     public $description;
-
     public $attributes_array;
     public $attribute_validations;
 
-    abstract public function mapValidation_Prefix_Attribute_Rules(callable $mappingFunction);
-    abstract public function setDefaultValidationRules();
+    abstract public function __construct(Array $attributes_array);
 
-    //quickly set all attributes from an Array
-    public function setPropertiesFrom_attributes_array(Array $attributes_array)
+    protected function setPropertiesFrom_attributes_array(Array $attributes_array)
     {
         foreach($attributes_array as $attribute=>$value){
             $this->$attribute = $value;
         }
-    }
-
-    public function changeValidationRule(String $prefix = null, String $attribute, String $newRule)
-    {
-        $key_exists = array_key_exists($prefix.$attribute, $this->attribute_validations);
-        if (!$key_exists){
-            return 'doesnt exist';
-        };
-
-        $this->attribute_validations[$prefix.$attribute] = $newRule;
-        return $this->attribute_validations[$prefix.$attribute];
     }
 }
 
---
+++ app/Classes/PageHelpers.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Classes;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
+
+class PageHelpers
+{
+    public static function mapValidation_Prefix_Attribute_Rules(callable $mappingFunction, Array $attributes_array, String $component_identifier){
+        $attribute_validations = [];
+
+        foreach($attributes_array as $attribute => $value){
+            $mappedValidationRule = $mappingFunction($attribute, $component_identifier);
+            array_push($attribute_validations, $mappedValidationRule);
+        };
+
+        $attribute_validations = Arr::collapse($attribute_validations);
+        return $attribute_validations;
+    }
+
+    public static function changeValidationRule(String $prefix = null, String $attribute, String $newRule)
+    {
+        $key_exists = array_key_exists($prefix.$attribute, $this->attribute_validations);
+        if (!$key_exists){
+            return 'doesnt exist';
+        };
+
+        $this->attribute_validations[$prefix.$attribute] = $newRule;
+        return $this->attribute_validations[$prefix.$attribute];
+    }
+}
