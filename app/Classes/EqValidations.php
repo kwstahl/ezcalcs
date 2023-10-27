5254df0b7570239e062c18c49c53986624240c50
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-10-27 20:00:16 +0000

    modified:   app/Classes/EqValidations.php

--- app/Classes/EqValidations.php
+++ app/Classes/EqValidations.php
@@ -1,16 +1,34 @@
 <?php
 namespace App\Classes;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 
 class EqValidations
 {
-    //list of attributes that must be validated.
-    public function __construct(Array $attributes_to_validate){
-        $this->setPropertiesForValidation($attributes_to_validate);
-    }
+    public $validations;
 
-    protected function setPropertiesForValidation($attributes_to_validate){
+    //list of attributes that must be validated.
+    public function setRequiredValidations($attributes_to_validate){
         foreach($attributes_to_validate as $attribute=>$value){
             $this->$attribute = $value;
         }
+    }
+
+    //All properties have the same validation prefix and rule. Returns array of arrays of validation rules.
+    public function setSameValidationRulesOnAll(Array $attributes_to_validate, String $prefix=null, String $suffix=null, String $rule)
+    {
+        $validationRules = [];
+        foreach($attributes_to_validate as $attributeName){
+            $attributeValidation = [$prefix.'.'.$attributeName.'.'.$suffix => $rule];
+
+            $this->$attributeName = $attributeValidation;
+            array_push($validationRules, $attributeValidation);
+        }
+        return $validationRules;
+    }
+
+    public function collapseArray(Array $validationArray){
+        Arr::collapse($validationArray);
     }
 }
