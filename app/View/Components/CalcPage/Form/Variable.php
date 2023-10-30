a547798c29e1b91a455f139c8a05ae21218aa92c
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-10-30 18:47:04 +0000

    modified:   app/View/Components/CalcPage/Form/Variable.php

--- app/View/Components/CalcPage/Form/Variable.php
+++ app/View/Components/CalcPage/Form/Variable.php
@@ -9,17 +9,20 @@ use Illuminate\View\Component;
 
 class Variable extends SuperVariable
 {
+/*
     public $name;
     public $unit;
     public $unit_class;
     public $sympy_symbol;
     public $latex_symbol;
     public $input_value;
-
+*/
     /**
      * Create a new component instance.
      */
-    public function __construct($name)
+    public function __construct(
+        public $name
+    )
     {
         $this->name = $name;
         //$this->attributes_array = collect($attributes_array);
