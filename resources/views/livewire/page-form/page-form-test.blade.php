6e1124e62a974a6b3c7f0a022c9877f3fcb60202
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-10-31 21:01:36 +0000

    modified:   resources/views/livewire/page-form/page-form-test.blade.php

--- resources/views/livewire/page-form/page-form-test.blade.php
+++ resources/views/livewire/page-form/page-form-test.blade.php
@@ -83,7 +83,7 @@
             });
         </script>
     @endpush
-    <x-calc-page.form.unit-options :name="ham" :options-array="$testUnit" :base-option="ham"/>
+    <x-calc-page.form.unit-options :name="$message" :options-array="$testUnit" :base-option="$message"/>
 
 
     <h1 class="row display-6 text-align-center p-5 justify-content-center">
