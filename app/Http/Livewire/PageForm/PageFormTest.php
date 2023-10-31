e252e575588ebdd218c740bf3067da07f620ea9a
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-10-31 20:42:34 +0000

    modified:   app/Http/Livewire/PageForm/PageFormTest.php

--- app/Http/Livewire/PageForm/PageFormTest.php
+++ app/Http/Livewire/PageForm/PageFormTest.php
@@ -142,7 +142,7 @@ class PageFormTest extends Component
 
     public function call_variables()
     {
-        $unitTest = $this->units->where('unit_class', 'time')->all();
+        $unitTest = $this->units->where('unit_class', 'time')->get('attributes');
         dd($unitTest);
     }
 
