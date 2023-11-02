39640e933758662944cfd2bcfc02a30dc0aac3d8
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-11-02 20:44:49 +0000

    modified:   resources/views/components/calc-page/form/unit-options.blade.php

--- resources/views/components/calc-page/form/unit-options.blade.php
+++ resources/views/components/calc-page/form/unit-options.blade.php
@@ -12,13 +12,27 @@
         </h2>
     </div>
 
-    <select id = "{{ $name }}" >
+    <select id = "{{ $name }}">
         @foreach ($optionsArray as $option => $value)
             <option value={{ $getOption($option)->conversion_to_base }}>
                 {{ $getOption($option)->symbol }}
             </option>
         @endforeach
-        </div>
-    </select>
+</div>
+</select>
+
+<button class="btn dropdown-toggle dropdown-toggle-split bg-white" data-bs-toggle="dropdown" type="button">
+
+</button>
+
+<ul class="dropdown-menu">
+    @foreach ($optionsArray as $option => $value)
+        <li>
+            <button class="dropdown-item" type="button">
+                {{ $getOption($option)->symbol }}
+            </button>
+        </li>
+    @endforeach
+</ul>
 
 </div>
