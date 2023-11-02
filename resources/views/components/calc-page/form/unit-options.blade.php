de71cd5651cbf5a6412858c48415bd250b9f7ea1
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-11-02 20:47:39 +0000

    modified:   resources/views/components/calc-page/form/unit-options.blade.php

--- resources/views/components/calc-page/form/unit-options.blade.php
+++ resources/views/components/calc-page/form/unit-options.blade.php
@@ -21,18 +21,26 @@
 </div>
 </select>
 
-<button class="btn dropdown-toggle dropdown-toggle-split bg-white" data-bs-toggle="dropdown" type="button">
 
-</button>
+<div class="btn-group">
+    <button class="btn bg-white" type="button">
+            hallo
 
-<ul class="dropdown-menu">
-    @foreach ($optionsArray as $option => $value)
-        <li>
-            <button class="dropdown-item" type="button">
-                {{ $getOption($option)->symbol }}
-            </button>
-        </li>
-    @endforeach
-</ul>
+    </button>
 
+    <button class="btn dropdown-toggle dropdown-toggle-split bg-white" data-bs-toggle="dropdown"
+        type="button">
+
+    </button>
+
+    <ul class="dropdown-menu">
+        @foreach ($optionsArray as $option => $value)
+            <li>
+                <button class="dropdown-item" type="button">
+                    {{ $getOption($option)->symbol }}
+                </button>
+            </li>
+        @endforeach
+    </ul>
+</div>
 </div>
