62587f34be22e58860007ecd927e1c535bdcc27e
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-11-02 21:06:51 +0000

    modified:   resources/views/components/calc-page/form/unit-options.blade.php

--- resources/views/components/calc-page/form/unit-options.blade.php
+++ resources/views/components/calc-page/form/unit-options.blade.php
@@ -1,25 +1,3 @@
-<div>
-    <h1>{{ $name }}</h1>
-    @foreach ($optionsArray as $option => $value)
-        {{ $option }}
-    @endforeach
-
-
-    <div>
-        <h2>
-            {{ $baseOption }}
-            {{ $getOption('days')->type }}
-        </h2>
-    </div>
-
-    <select id = "{{ $name }}">
-        @foreach ($optionsArray as $option => $value)
-            <option value={{ $getOption($option)->conversion_to_base }}>
-                {{ $getOption($option)->symbol }}
-            </option>
-        @endforeach
-    </select>
-</div>
 
 <div class="col-4 dropdown">
     <button class="btn bg-white dropdown-toggle" type="button" data-bs-toggle="dropdown">
