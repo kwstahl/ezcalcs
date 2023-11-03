0624176cf29f319c4278e4c48fcdd09fbff39aab
Kevin_Stahl <kevin.sthl97@gmail.com>
2023-11-03 21:44:02 +0000

    new file:   app/Http/Livewire/PageComponent/UnitOptions.php 	new file:   resources/views/livewire/page-component/unit-options.blade.php

---
+++ app/Http/Livewire/PageComponent/UnitOptions.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\PageComponent;
+
+use Livewire\Component;
+
+class UnitOptions extends Component
+{
+    public function render()
+    {
+        return view('livewire.page-component.unit-options');
+    }
+}
