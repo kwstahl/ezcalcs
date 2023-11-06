ddd42ae87009cfa03e0f5ff2ecfd1e1e823cf74f
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-11-06 22:00:28 +0000

    modified:   resources/views/livewire/page-component/unit-options.blade.php

--- resources/views/livewire/page-component/unit-options.blade.php
+++ resources/views/livewire/page-component/unit-options.blade.php
@@ -9,7 +9,7 @@
     <ul class="dropdown-menu" id="dropdown-container">
         @foreach ($optionsArray as $option => $value)
             <li>
-                <button class="dropdown-item" wire:click="$emitSelf('ugh')">
+                <button class="dropdown-item" wire:click="$emitSelf('ugh')" :wire:key="{{ $option->id }}">
                     {{ $this->$option->symbol }}
                 </button>
             </li>
