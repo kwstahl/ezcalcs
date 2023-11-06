acb8edb90a5ff99d9f86e89b920c05c6c39bc6a5
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-11-06 22:39:32 +0000

    modified:   resources/views/livewire/page-component/unit-options.blade.php

--- resources/views/livewire/page-component/unit-options.blade.php
+++ resources/views/livewire/page-component/unit-options.blade.php
@@ -8,7 +8,7 @@
         @foreach ($optionsArray as $option => $value)
             <li>
                 <button wire:click="changeSelectedOption('days')" class="dropdown-item">
-                    {{ dump($this->option) }}
+                    {{ dump($this->$option) }}
                     {{ $this->$option->symbol }}
                     {{ $this->$option->id }}
                 </button>
