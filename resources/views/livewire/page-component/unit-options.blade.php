0505196198fac5e478e1440bc1dd46f937fa2d8c
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-11-06 21:38:46 +0000

    modified:   resources/views/livewire/page-component/unit-options.blade.php

--- resources/views/livewire/page-component/unit-options.blade.php
+++ resources/views/livewire/page-component/unit-options.blade.php
@@ -1,13 +1,15 @@
 <div class="col-4 dropdown">
     <button class="btn bg-white dropdown-toggle" type="button" data-bs-toggle="dropdown">
         hallo
-        {{ $selectedOption->symbol }}
+        @isset($selectedOption)
+            {{ $selectedOption->symbol }}
+        @endisset
     </button>
 
     <ul class="dropdown-menu" id="dropdown-container">
         @foreach ($optionsArray as $option => $value)
             <li>
-                <button class="dropdown-item">
+                <button class="dropdown-item" wire:click="setSelectedOption({{ $this->$option->id }})">
                     {{ $this->$option->symbol }}
                 </button>
             </li>
