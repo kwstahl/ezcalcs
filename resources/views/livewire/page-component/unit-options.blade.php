8516335cc7e6a77373d12fa0ae8dc3a1dfbd29e0
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-11-06 20:56:10 +0000

    modified:   resources/views/livewire/page-component/unit-options.blade.php

--- resources/views/livewire/page-component/unit-options.blade.php
+++ resources/views/livewire/page-component/unit-options.blade.php
@@ -1,3 +1,26 @@
-<div>
-Hello
+<div class="col-4 dropdown">
+    <button class="btn bg-white dropdown-toggle" type="button" data-bs-toggle="dropdown">
+        hallo
+        @isset($selectedOption)
+            {{ $selectedOption->symbol }}
+        @endisset
+    </button>
+
+<div id="test">
+scoop
+</div>
+
+<div id="days">
+    ham
+</div>
+
+    <ul class="dropdown-menu" id="dropdown-container">
+        @foreach ($optionsArray as $option => $value)
+            <li>
+                <button class="dropdown-item">
+                    {{ $getOption($option)->symbol }}
+                </button>
+            </li>
+        @endforeach
+    </ul>
 </div>
