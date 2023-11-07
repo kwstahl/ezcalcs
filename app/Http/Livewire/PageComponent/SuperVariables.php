a16d42a5002fea22ae27de826772a409b8ce9e9d
Kevin_Stahl <kevin.sthl97@gmail.com>
2023-11-07 22:20:58 +0000

    new file:   app/Http/Livewire/PageComponent/SuperVariables.php 	new file:   app/Http/Livewire/PageComponent/Variables.php 	new file:   resources/views/livewire/page-component/super-variables.blade.php 	new file:   resources/views/livewire/page-component/variables.blade.php

---
+++ app/Http/Livewire/PageComponent/SuperVariables.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\PageComponent;
+
+use Livewire\Component;
+
+class SuperVariables extends Component
+{
+    public function render()
+    {
+        return view('livewire.page-component.super-variables');
+    }
+}
---
+++ app/Http/Livewire/PageComponent/Variables.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\PageComponent;
+
+use Livewire\Component;
+
+class Variables extends Component
+{
+    public function render()
+    {
+        return view('livewire.page-component.variables');
+    }
+}
