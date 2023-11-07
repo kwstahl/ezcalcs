3e1765dd73d87bb4ce570e2ee551fa6f8f9e9ea2
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-11-07 22:09:43 +0000

    modified:   resources/views/livewire/page-component/unit-options.blade.php

--- resources/views/livewire/page-component/unit-options.blade.php
+++ resources/views/livewire/page-component/unit-options.blade.php
@@ -7,7 +7,7 @@
     <ul class="dropdown-menu" id="dropdown-container">
         @foreach ($optionsArray as $option => $value)
             <li>
-                <button class="dropdown-item" wire:click="changeSelectedOption('{{ $this->option['id'] }}')">
+                <button class="dropdown-item" wire:click="changeSelectedOption('{{ $this->$option['id'] }}')">
                     {{ $this->$option['symbol'] }}
                     {{ $this->$option['id'] }}
                 </button>
