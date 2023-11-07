0b6cf4144f720053b37bf0023352daf6709f389f
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-11-07 22:19:36 +0000

    modified:   app/Http/Livewire/PageComponent/UnitOptions.php

--- app/Http/Livewire/PageComponent/UnitOptions.php
+++ app/Http/Livewire/PageComponent/UnitOptions.php
@@ -11,7 +11,6 @@ class UnitOptions extends SuperOptions
     public $optionsArray;
     public $baseOption;
 
-
     /**
      * Create a new component instance.
      */
@@ -28,7 +27,6 @@ class UnitOptions extends SuperOptions
     {
         $optionObject = $this->getOptionObjectFromOptionsArray($optionId);
         $this->selectedOption = $optionObject;
-        
     }
 
     /**
