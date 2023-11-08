0a36a7a9b380f0b9ec3883105b94938427d987a8
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-11-08 22:10:37 +0000

    modified:   resources/views/livewire/page-component/variables.blade.php

--- resources/views/livewire/page-component/variables.blade.php
+++ resources/views/livewire/page-component/variables.blade.php
@@ -1,12 +1,2 @@
-<div>
-    hi
-    {{ $this->unit }}
-    {{ $this->unit = 'ham' }}
-    {{ $this->unit }}
-    {{ $this->description }}
-
-    {{ $this->latex_symbol }}
-</div>
-
 <input class="form-control" type="text" name="{{ $name }}">
 <label>{{ $name }} ({{ $this->latex_symbol }}) </label>
