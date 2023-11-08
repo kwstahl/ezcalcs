191aa1f759bed5fd86c553867f689109e64d01c2
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-11-08 22:11:17 +0000

    modified:   resources/views/components/calc-page/form/variable.blade.php

--- resources/views/components/calc-page/form/variable.blade.php
+++ resources/views/components/calc-page/form/variable.blade.php
@@ -1,2 +1,4 @@
-<input class="form-control" type="text" name="{{ $name }}">
-<label wire:ignore>{{ $name }} ({{ $latex_symbol }}) </label>
+<div class="form-floating">
+    <input class="form-control" type="text" name="{{ $name }}">
+    <label wire:ignore>{{ $name }} ({{ $latex_symbol }}) </label>
+</div>
