0e89a1a471612830e3e8275dc9556ba1589544d0
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-11-09 22:55:19 +0000

    modified:   resources/views/livewire/page-component/variables.blade.php

--- resources/views/livewire/page-component/variables.blade.php
+++ resources/views/livewire/page-component/variables.blade.php
@@ -1,2 +1,2 @@
-<input class="form-control" type="text" name="{{ $name }}" wire:model="{{ $wireModel }}">
+<input class="form-control" type="text" name="{{ $name }}">
 <label>{{ $name }} ({{ $this->latex_symbol }}) </label>
