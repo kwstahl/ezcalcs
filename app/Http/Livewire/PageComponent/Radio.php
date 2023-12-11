5eecee5d3e91091fbfc343f39a0e6d9a24b17ad0
Kevin_Stahl <kevin.sthl97@gmail.com>
2023-12-11 20:23:04 +0000

    new file:   app/Http/Livewire/PageComponent/Radio.php 	new file:   resources/views/livewire/page-component/radio.blade.php

---
+++ app/Http/Livewire/PageComponent/Radio.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\PageComponent;
+
+use Livewire\Component;
+
+class Radio extends Component
+{
+    public function render()
+    {
+        return view('livewire.page-component.radio');
+    }
+}
