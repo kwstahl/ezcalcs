1432d8e65bc72c25db1eefa2b2af633063d0a8d6
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-12-18 22:54:14 +0000

    modified:   resources/views/livewire/page-form/page-form-test.blade.php

--- resources/views/livewire/page-form/page-form-test.blade.php
+++ resources/views/livewire/page-form/page-form-test.blade.php
@@ -2,10 +2,12 @@
     <form wire:submit.prevent="submit">
         <!-- Input Group Row Created for each variable -->
         @foreach ($variables_json as $variableName => $variable)
+        <div wire:key="{{$variableName}}">
             <!--  Radio  -->
             <livewire:page-component.radio :attributes-array="$variables_json[$variableName]" />
             <!-- Input Text -->
             <livewire:page-component.variables :attributes-array="$variable" />
+        </div>
         @endforeach
     </form>
     <h1 class="row display-6 text-align-center p-5 justify-content-center">
