18598f3ce917e6dc18e7756a9101341c84a4b6d2
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2023-12-28 22:31:29 +0000

    modified:   resources/views/livewire/page-component/variables.blade.php

--- resources/views/livewire/page-component/variables.blade.php
+++ resources/views/livewire/page-component/variables.blade.php
@@ -1,2 +1,4 @@
-<input class="form-control" type="text" wire:model.defer="{{ $this->inputValue }}" @disabled($this->disabled) wire:key="{{$this->sympy_symbol}}">
-<label>{{ $this->sympy_symbol }} ({{ $this->latex_symbol }}) hi </label>
+<div wire:key="{{$this->sympy_symbol}}">
+    <input class="form-control" type="text" wire:model.defer="{{ $this->inputValue }}" @disabled($this->disabled)>
+    <label>{{ $this->sympy_symbol }} ({{ $this->latex_symbol }}) hi </label>
+</div>
