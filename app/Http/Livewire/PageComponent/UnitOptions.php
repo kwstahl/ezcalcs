e7617c9ba2b4724ab89c5356848e4b8c6103e822
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2024-01-03 22:28:20 +0000

    modified:   app/Http/Livewire/PageComponent/UnitOptions.php

--- app/Http/Livewire/PageComponent/UnitOptions.php
+++ app/Http/Livewire/PageComponent/UnitOptions.php
@@ -46,9 +46,7 @@ class UnitOptions extends SuperOptions implements Validation
     //returns an array of objects based on the model attribute and the value to filter from
     public static function createOptionsArray($attributeValue)
     {
-        $allUnits = Unit::all();
-
-
+        $allUnits = Unit::where('unit_class', 'time')->get();
         return $allUnits;
     }
 
