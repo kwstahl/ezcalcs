ebf578291e97c6f99303cab2a80a445a85642362
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2024-01-03 17:45:12 +0000

    modified:   app/Http/Livewire/PageComponent/Variables.php

--- app/Http/Livewire/PageComponent/Variables.php
+++ app/Http/Livewire/PageComponent/Variables.php
@@ -32,9 +32,9 @@ class Variables extends SuperVariables
 
     protected function rules() {
         if ($this->disabled == false){
-            return [$this->inputValue, 'nullable'];
+            return [$this->inputValue => 'nullable'];
         } else {
-            return [$this->inputValue, 'required|numeric'];
+            return [$this->inputValue => 'required|numeric'];
         }
     }
 
