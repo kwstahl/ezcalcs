5fcd8acb17d63c2629c65d3c314fc820080fa9b7
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2024-01-03 17:34:37 +0000

    modified:   app/Http/Livewire/PageComponent/Variables.php

--- app/Http/Livewire/PageComponent/Variables.php
+++ app/Http/Livewire/PageComponent/Variables.php
@@ -40,6 +40,7 @@ class Variables extends SuperVariables
 
     public function disable($name){
         if ($name == $this->name){
+            unset($this->inputValue);
             $this->disabled = true;
         } else {
             $this->disabled = false;
