d961876483a2be97d055a51c1360640efc24bae9
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2024-01-03 18:33:14 +0000

    modified:   resources/views/livewire/page-component/variables.blade.php

--- resources/views/livewire/page-component/variables.blade.php
+++ resources/views/livewire/page-component/variables.blade.php
@@ -1,5 +1,10 @@
-<div wire:key="{{$this->name}}-variable">
+<div wire:key="{{ $this->name }}-variable">
     <input class="form-control" type="text" wire:model="inputValue" @disabled($this->disabled)>
     <label wire:ignore>{{ $this->sympy_symbol }} ({{ $this->latex_symbol }}) hi </label>
-    @error('inputValue') <span class="error">{{ $message }}</span> @enderror
+
+    @error('inputValue')
+        <div class="invalid-feedback">
+            <span class="error">{{ $message }}</span>
+        </div>
+    @enderror
 </div>
