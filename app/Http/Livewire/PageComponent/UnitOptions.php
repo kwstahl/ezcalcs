c4f440f47319f7e367b3edec8d447c2d73e2c787
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2024-01-03 22:42:48 +0000

    modified:   app/Http/Livewire/PageComponent/UnitOptions.php

--- app/Http/Livewire/PageComponent/UnitOptions.php
+++ app/Http/Livewire/PageComponent/UnitOptions.php
@@ -46,10 +46,11 @@ class UnitOptions extends SuperOptions implements Validation
     //returns an array of objects based on the model attribute and the value to filter from
     public static function createOptionsArray($attributeValue)
     {
-        $allUnits = Unit::all();
+        $allUnits = Unit::where('unit_class', $attributeValue);
         $keyedCollection = $allUnits->mapWithKeys(function($item, $key){
             return [$item->id => $item];
         })->all();
+
         $keyed = collect($keyedCollection);
         $wanted = $keyed->where('unit_class', $attributeValue);
         return $wanted;
