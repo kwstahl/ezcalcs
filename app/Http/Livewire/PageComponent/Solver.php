833734a9f9f5334198ed1b5d9dfee010ba950aef
Kevin_Stahl <kevin.sthl97@gmail.com>
2024-01-05 22:20:06 +0000

    new file:   app/Http/Livewire/PageComponent/Solver.php 	new file:   resources/views/livewire/page-component/solver.blade.php

---
+++ app/Http/Livewire/PageComponent/Solver.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\PageComponent;
+
+use Livewire\Component;
+
+class Solver extends Component
+{
+    public function render()
+    {
+        return view('livewire.page-component.solver');
+    }
+}
