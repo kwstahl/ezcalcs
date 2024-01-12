643b888148fa5b109ebebc23aa0562422a74c15a
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2024-01-12 22:21:04 +0000

    modified:   app/Http/Livewire/PageComponent/Variables.php

--- app/Http/Livewire/PageComponent/Variables.php
+++ app/Http/Livewire/PageComponent/Variables.php
@@ -62,7 +62,7 @@ class Variables extends SuperVariables implements Validation
     public function sendData()
     {
         $preparedData = [$this->sympy_symbol => $this->inputValue];
-        $this->emit('sendData', $preparedData);
+        $this->emit('sendData', $preparedData, 'value');
     }
 
     public function render()
