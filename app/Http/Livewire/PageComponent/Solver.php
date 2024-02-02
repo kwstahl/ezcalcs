9bf8ad71005bb6ab123ee80ce81319ef23f333fe
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2024-02-02 21:25:05 +0000

    modified:   app/Http/Livewire/PageComponent/Solver.php

--- app/Http/Livewire/PageComponent/Solver.php
+++ app/Http/Livewire/PageComponent/Solver.php
@@ -16,16 +16,27 @@ class Solver extends Component
     {
         $this->sympyDataArray = [];
         $this->variableCollection = $this->makeSympyPreparationCollection();
+        $this->createVariableVectors();
     }
 
     protected $listeners = [
         'sendData' => 'pushData',
     ];
 
+    //this function is meant to create a vector for each variable like: var1 = [value, unit_conversion]
+    public function createVariableVectors()
+    {
+        foreach($this->variablesJson as $variableName => $variableArray){
+            $sympy_symbol = $variableArray['sympy_symbol'];
+            $this->$sympy_symbol = ['value' => '', 'unit_conversion' => ''];
+        };
+        dump($this->$sympy_symbol);
+    }
+
     public function makeSympyPreparationCollection()
     {
         $variablesJson = collect($this->variablesJson);
-        $emptyCollection = $variablesJson->mapWithKeys(function($variableItem, $variableName){
+        $emptyCollection = $variablesJson->mapWithKeys(function ($variableItem, $variableName) {
             return [$variableItem['sympy_symbol'] => ['value' => null, 'unit_conversion' => null]];
         });
         return $emptyCollection;
@@ -33,13 +44,11 @@ class Solver extends Component
 
     public function pushData($name, $type, $value)
     {
-        //$this->variableCollection = $this->variableCollection->merge([$name => [$type, $value]]);
-       $variableCollection = $this->variableCollection;
-       $variableCollection[$name][$type] = $value;
-       $this->variableCollection = $variableCollection;
+        $this->variableCollection = $this->variableCollection->merge([$name => [$type, $value]]);
     }
 
-    public function checkProgress(){
+    public function checkProgress()
+    {
         $this->emit('validationEvent');
         dump($this->variableCollection);
     }
