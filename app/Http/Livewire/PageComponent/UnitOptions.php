4789b8171278fb1d7d9ade567fc5accd11f84b9d
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2024-02-02 22:32:24 +0000

    modified:   app/Http/Livewire/PageComponent/UnitOptions.php

--- app/Http/Livewire/PageComponent/UnitOptions.php
+++ app/Http/Livewire/PageComponent/UnitOptions.php
@@ -45,6 +45,7 @@ class UnitOptions extends SuperOptions implements Validation
         $optionObject = $this->getOptionObjectFromOptionsArray($optionId);
         $this->selectedOption = $optionObject;
         $this->emit('sendData', $this->variableSympySymbol, 'unit_conversion', $optionObject['conversion_to_base']);
+        dump($this->variableSympySymbol);
     }
 
     public function validation()
