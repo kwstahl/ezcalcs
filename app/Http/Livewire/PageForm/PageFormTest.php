c80c39f2c6a6945760824e08fb3ff9037777c3f6
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2024-02-13 22:12:30 +0000

    modified:   app/Http/Livewire/PageForm/PageFormTest.php 	modified:   resources/views/livewire/page-component/solver.blade.php

--- app/Http/Livewire/PageForm/PageFormTest.php
+++ app/Http/Livewire/PageForm/PageFormTest.php
@@ -13,37 +13,11 @@ use App\Http\Livewire\PageComponent\UnitOptions;
 
 class PageFormTest extends Component
 {
-    public $variables_json;
-    public $units;
-    public $unitOptions;
-    public $variableInputData;
-    public $formula_sympy;
     public $variableToSolveFor;
-    public $answer;
-    public $variables;
 
     public function mount()
     {
-        $this->units = Unit::all();
-        $this->unitOptions = collect();
-
-        $this->variables_json = collect($this->variables_json);
-        $this->variableInputData = collect();
-        $this->variableToSolveFor = $this->variables_json->keys()->first();
-
-        $this->answer = '';
-        $this->errorOut = '';
-
-        $this->setVariableInputData();
-        $this->setUnitOptionsForEachVariable();
-
-
-        $this->unitTest = PageHelpers::setIdsOnCollectionOfModels($this->units, 'id');
-        $unitTest = $this->unitTest->where('unit_class', 'time')->all();
     }
-
-    protected $messages = [
-    ];
 
     protected $listeners = [
         'radioSelected' => 'setVariableToSolveFor',
@@ -51,97 +25,6 @@ class PageFormTest extends Component
 
     public function setVariableToSolveFor($name){
         $this->variableToSolveFor = $name;
-    }
-
-    public function unitSelected($unitIndex, $variableName)
-    {
-        $this->variableInputData[$variableName]['unit_conversion'] = 'hi';
-    }
-
-    protected function rules()
-    {
-        $variableToSolveForValueEntry = 'variableInputData.' . $this->variableToSolveFor . '.Value';
-        return [
-            'variableInputData.*.Value' => 'required|numeric',
-            $variableToSolveForValueEntry => 'nullable',
-            'variableInputData.*.unit_conversion' => 'required|numeric',
-            'variableToSolveFor' => 'required',
-        ];
-    }
-
-    private function setVariableInputData()
-    {
-        $this->variableInputData = $this->variables_json->mapWithKeys(function ($variable, $variableName) {
-            return [
-                $variableName => [
-                    'sympy_symbol' => $variable['sympy_symbol'],
-                    'Value' => '',
-                    'unit_conversion' => '',
-                ]
-            ];
-        });
-    }
-
-    private function setUnitOptionsForEachVariable()
-    {
-        foreach ($this->variables_json as $variableName => $variable) {
-            $variableUnitClass = $variable['unit'];
-            $unitsForVariable = $this->units
-                ->where('unit_class', $variableUnitClass)
-                ->mapWithKeys(function ($unit, $unitName) {
-                    return [
-                        $unit->id => [
-                            'symbol' => $unit->symbol,
-                            'conversion_to_base' => $unit->conversion_to_base,
-                            'unit_class' => $unit->unit_class,
-                        ]
-                    ];
-                })
-                ->all();
-
-            $this->unitOptions[$variableName] = $unitsForVariable;
-        }
-    }
-
-    public function updatedVariableToSolveFor()
-    {
-        /* Laravel's collection class is immutable, so a new collection instance with a cleared value must be created, then assigned */
-        $newVariableToSolveFor = $this->variableInputData[$this->variableToSolveFor];
-        $newVariableToSolveFor['Value'] = '';
-        $this->variableInputData[$this->variableToSolveFor] = $newVariableToSolveFor;
-        $this->rules();
-    }
-
-    public function submit()
-    {
-        $this->validate();
-        $dataForSympyInJson = $this->prepareDataForSympyInJson();
-        $this->sendDataToSympy($dataForSympyInJson);
-    }
-
-    private function prepareDataForSympyInJson()
-    {
-
-        $dataForSympyInJson = $this->variableInputData;
-        $dataForSympyInJson = $this->variableInputData->mapWithKeys(function ($variable, $variableName) {
-            $sympy_symbol = $variable['sympy_symbol'];
-            return
-                [
-                    $sympy_symbol => [
-                        'Value' => $variable['Value'],
-                        'unit_conversion' => (float) $variable['unit_conversion'],
-                    ]
-                ];
-        });
-        return $dataForSympyInJson;
-    }
-
-    private function sendDataToSympy($dataForSympyInJson)
-    {
-        $command = 'python3 sympyScript.py' . ' ' . escapeshellarg($dataForSympyInJson) . ' ' . escapeshellarg($this->formula_sympy);
-        $this->answer = Process::run($command)->output();
-        $this->errorOut = Process::run($command)->errorOutput();
-        $this->render();
     }
 
     public function render()
--- resources/views/livewire/page-component/solver.blade.php
+++ resources/views/livewire/page-component/solver.blade.php
@@ -16,7 +16,6 @@
             {{ $errorOut }}
         @endisset
     </div>
-
 </div>
 
 
