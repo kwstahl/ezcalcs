0e128074cb65df008f5f2cb1243e33b97c4dcc2d
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2024-02-13 22:20:37 +0000

    modified:   resources/views/livewire/page-form/page-form-test.blade.php

--- resources/views/livewire/page-form/page-form-test.blade.php
+++ resources/views/livewire/page-form/page-form-test.blade.php
@@ -5,21 +5,19 @@
             <div>
                 <!--  Radio  -->
                 <livewire:page-component.radio :attributes-array="$variable" />
-                <!-- Input Text -->
+
+                <!-- Input -->
                 <livewire:page-component.variables :attributes-array="$variable" />
-                <livewire:page-component.unit-options :unit-name="$variable['unit']" :variable-sympy-symbol="$variable['sympy_symbol']"/>
+
+                <!-- Unit -->
+                <livewire:page-component.unit-options :unit-name="$variable['unit']" :variable-sympy-symbol="$variable['sympy_symbol']" />
             </div>
         @endforeach
     </form>
+
     <h1 class="row display-6 text-align-center p-5 justify-content-center">
         You are solving for {{ $variableToSolveFor }}
     </h1>
 
-    <div>
-
-    </div>
-
-    <livewire:page-component.solver :variables-json="$variables_json" :formula-sympy="$formula_sympy"/>
-
-    <div>
-    </div>
+    <livewire:page-component.solver :variables-json="$variables_json" :formula-sympy="$formula_sympy" />
+</div>
