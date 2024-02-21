dc188cecf3562f54b96142ca745dd78805a20d20
Kevin Stahl <117512332+kwstahl@users.noreply.github.com>
2024-02-21 22:51:15 +0000

    modified:   app/Http/Livewire/PageComponent/Variables.php

--- app/Http/Livewire/PageComponent/Variables.php
+++ app/Http/Livewire/PageComponent/Variables.php
@@ -53,7 +53,7 @@ class Variables extends SuperVariables implements Validation
 
     public function validation()
     {
-        $this->withValidator(function (Validator $validator) {dump($validator->errors());});
+        $this->withValidator(function (Validator $validator) {$val = $validator->errors();})->validate();
     }
 
     public function __get($attribute)
